f9df44dda0ac9261a6927cde95a25ffbc37321d2
breadrock <breadrock1@gmail.com>
2021-02-28 20:24:48 +0300

    Has been fixed bugs with exceptions

--- Scrapers/Vkontakte/VkScraper.py
+++ Scrapers/Vkontakte/VkScraper.py
@@ -1,15 +1,6 @@
-"""
-This request url help u get user access token for Vk account if dis token is need.
-
-scope = 'photos,audio,video,docs,notes,pages,status,offers,questions,wall,' \
-        'groups,email,notifications,stats,ads,offline,docs,pages,stats,notifications'
-
-oauth = f'https://oauth.vk.com/authorize?client_id=7752097&scope={scope}&response_type=token'
-"""
-
 from typing import Dict
-from requests import post
 from logging import info, warning
+from requests import post, RequestException
 
 from config import (
     VK_APP_VERSION,
@@ -28,18 +19,28 @@ class VkScraper(object):
 
         self.parsed_data = {}
 
-    def __get_vk_data(self, method: str, data: Dict[str, str]) -> Dict[str, str or Dict]:
-        response = post(
-            url=f'https://api.vk.com/method/{method}',
-            data=data,
-            verify=False,
-            allow_redirects=False
-        )
+    def __extract_data(self, data: Dict[str, str or Dict], key: str) -> Dict:
+        return data.get(key) if key in data else ''
 
-        if response.status_code != OK_STATUS_CODE:
-            return {}
+    def __get_vk_data(self, method: str, data: Dict[str, str or Dict]) -> Dict[str, str or Dict] or None:
+        try:
+            response = post(
+                url=f'https://api.vk.com/method/{method}',
+                data=data,
+                verify=False,
+                allow_redirects=False
+            )
+        except RequestException as e:
+            warning(msg=f'[!]\tFailed to send request: {e.strerror}')
+            return None
 
-        return response.json()
+        try:
+            json_data = response.json().get('response')
+        except KeyError as e:
+            warning(msg=f'[!]\tFailed to send request: {e}')
+            return None
+
+        return json_data
 
     def get_parsed_data(self) -> Dict[str, str or Dict]:
         return self.parsed_data
@@ -49,7 +50,7 @@ class VkScraper(object):
         info(msg='[*]\tStarting the Vkontakte scraping process...', level=0)
 
         # first name, last name, date of birth, city
-        user_info = self.__get_vk_data(
+        vk_data = self.__get_vk_data(
             method='users.get',
             data={
                 'user_ids': user,
@@ -57,33 +58,38 @@ class VkScraper(object):
                 'access_token': self.access_token,
                 'v': self.app_version
             }
-        ).get('response')[0]
-        self.parsed_data.update(user_info)
+        )
+        user_info = vk_data[0]
+
+        first_name = self.__extract_data(data=user_info, key='first_name')
+        last_name = self.__extract_data(data=user_info, key='last_name')
+        birthdate = self.__extract_data(data=user_info, key='bdate')
+        hometown = self.__extract_data(data=user_info.get('city'), key='title')
 
         # friends
-        user_friends = self.__get_vk_data(
+        vk_data = self.__get_vk_data(
             method='friends.get',
             data={
                 'user_id': user,
                 'access_token': self.access_token,
                 'v': self.app_version
             }
-        ).get('response').get('items')
-        self.parsed_data.update({'friends': user_friends})
+        )
+        user_friends = [] if vk_data is None else self.__extract_data(data=vk_data, key='items')
 
         # followers
-        user_followers = self.__get_vk_data(
+        vk_data = self.__get_vk_data(
             method='users.getFollowers',
             data={
                 'user_id': user,
                 'access_token': self.access_token,
                 'v': self.app_version
             }
-        ).get('response').get('items')
-        self.parsed_data.update({'followers': user_followers})
+        )
+        user_followers = [] if vk_data is None else self.__extract_data(data=vk_data, key='items')
 
         # groups
-        user_groups = self.__get_vk_data(
+        vk_data = self.__get_vk_data(
             method='groups.get',
             data={
                 'user_id': user,
@@ -92,11 +98,11 @@ class VkScraper(object):
                 'access_token': self.access_token,
                 'v': self.app_version
             }
-        ).get('response').get('items')
-        self.parsed_data.update({'groups': user_groups})
+        )
+        user_groups = [] if vk_data is None else self.__extract_data(data=vk_data, key='items')
 
         # posts
-        user_wall = self.__get_vk_data(
+        vk_data = self.__get_vk_data(
             method='wall.get',
             data={
                 'owner_id': user,
@@ -105,53 +111,51 @@ class VkScraper(object):
                 'access_token': self.access_token,
                 'v': self.app_version
             }
-        ).get('response').get('items')
-        self.parsed_data.update({'posts': []})
+        )
+        user_wall = [] if vk_data is None else self.__extract_data(data=vk_data, key='items')
 
+        user_post: Dict
         for user_post in user_wall:
             post_id = user_post.get('id')
 
-            try:
-                user_post.pop('id')
-                user_post.pop('attachments')
-            except KeyError:
-                warning(msg=f'[!]\tWarning! The post: \"{post_id}\" has no any attachments.')
-
             # reposts
-            try:
-                user_reposts = self.__get_vk_data(
-                    method='wall.getReposts',
-                    data={
-                        'owner_id': user,
-                        'post_id': post_id,
-                        'access_token': self.access_token,
-                        'v': self.app_version
-                    }
-                ).get('response').get('items')
-            except AttributeError:
-                warning(msg=f'[!]\tWarning! There are no any reports for post: \"{post_id}\".')
-                user_reposts = []
+            vk_data = self.__get_vk_data(
+                method='wall.getReposts',
+                data={
+                    'owner_id': user,
+                    'post_id': post_id,
+                    'access_token': self.access_token,
+                    'v': self.app_version
+                }
+            )
+            user_reposts = [] if vk_data is None else self.__extract_data(data=vk_data, key='items')
 
             #  comments
-            try:
-                user_comments = self.__get_vk_data(
-                    method='wall.getComments',
-                    data={
-                        'owner_id': user,
-                        'post_id': post_id,
-                        'extended': 1,
-                        'filter': 'all',
-                        'access_token': self.app_service,
-                        'v': self.app_version
-                    }
-                ).get('response').get('items')
-            except AttributeError:
-                warning(msg=f'[!]\tWarning! There are no any comments for post: \"{post_id}\".')
-                user_comments = []
+            vk_data = self.__get_vk_data(
+                method='wall.getComments',
+                data={
+                    'owner_id': user,
+                    'post_id': post_id,
+                    'extended': 1,
+                    'filter': 'all',
+                    'access_token': self.app_service,
+                    'v': self.app_version
+                }
+            )
+            user_comments = [] if vk_data is None else self.__extract_data(data=vk_data, key='items')
 
-            user_post.get('comments').update({'commenters': user_comments})
             user_post.get('reposts').update({'reporters': user_reposts})
+            user_post.get('comments').update({'commenters': user_comments})
 
-            self.parsed_data.get('posts').append({post_id: user_post})
+        self.parsed_data.update({
+            'first_name': first_name,
+            'last_name' : last_name,
+            'birthday'  : birthdate,
+            'city'      : hometown,
+            'friends'   : user_friends,
+            'followers' : user_followers,
+            'groups'    : user_groups,
+            'posts'     : user_wall
+        })
 
         info(msg='[+]\tThe Vkontakte scraping process has been done!', level=0)
