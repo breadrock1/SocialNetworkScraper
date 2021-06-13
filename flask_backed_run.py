3d666d2395bef68b29c0fc10ab4adb44e3ff0b63
breadrock <breadrock1@gmail.com>
2021-06-13 18:38:22 +0300

    Has been added ability to launch scraping in dif modes

--- flask_backed_run.py
+++ flask_backed_run.py
@@ -18,26 +18,59 @@ def loadJsonDataFromRequest(data: Union[bytes, str]) -> json:
         return {'result': -1, 'message': e.msg}
 
 
-def launchScrapingProcess(credentials: json) -> Dict:
+def launchScrapingProcess(credentials: json, mode: str) -> Dict:
     scraped_data = {}
 
-    scraped_data.update(
-        ScraperManager.scrapeOSINTSites(credentials=credentials)
-    )
-    scraped_data.update(
-        ScraperManager.scrapeSocialNetworks(credentials=credentials)
-    )
+    if mode == '/social_scraping':
+        scraped_data.update(
+            ScraperManager.scrapeSocialNetworks(credentials=credentials)
+        )
+
+    elif mode == 'osint_scraping':
+        scraped_data.update(
+            ScraperManager.scrapeOSINTSites(credentials=credentials)
+        )
+
+    else:
+        scraped_data.update(
+            ScraperManager.scrapeSocialNetworks(credentials=credentials)
+        )
+        scraped_data.update(
+            ScraperManager.scrapeOSINTSites(credentials=credentials)
+        )
 
     return scraped_data
 
 
-@app.route('/full_osint', methods=['POST'])
-def startFullOsintModule():
+@app.route('/full_scraping', methods=['POST'])
+def launchFullScraping():
     if request.method == 'POST':
         data = request.get_data().decode('UTF-8')
         user_creds = loadJsonDataFromRequest(data)
 
-        return launchScrapingProcess(credentials=user_creds)
+        return launchScrapingProcess(credentials=user_creds, mode='full_scraping')
+
+    abort(405)
+
+
+@app.route('/social_scraping', methods=['POST'])
+def launchSocialScraping():
+    if request.method == 'POST':
+        data = request.get_data().decode('UTF-8')
+        user_creds = loadJsonDataFromRequest(data)
+
+        return launchScrapingProcess(credentials=user_creds, mode='social_scraping')
+
+    abort(405)
+
+
+@app.route('/osint_scraping', methods=['POST'])
+def launchOsintScraping():
+    if request.method == 'POST':
+        data = request.get_data().decode('UTF-8')
+        user_creds = loadJsonDataFromRequest(data)
+
+        return launchScrapingProcess(credentials=user_creds, mode='osint_scraping')
 
     abort(405)
 
