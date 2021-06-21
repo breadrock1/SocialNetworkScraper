42a51d28d8e723a7c212639481f90c162a31e681
breadrock <breadrock1@gmail.com>
2021-06-21 20:11:46 +0300

    Has been fixed some bugs also has been optimized some code

--- flask_backed_run.py
+++ flask_backed_run.py
@@ -10,15 +10,6 @@ from Scrapers.SraperManager import ScraperManager
 app = Flask(__name__)
 
 
-def loadJsonDataFromRequest(data: Union[bytes, str]) -> json:
-    try:
-        return json.loads(data)
-
-    except json.JSONDecodeError as e:
-        print(f'[-]\tFailed to decode json data. Error: {e.msg}')
-        return {'result': -1, 'message': e.msg}
-
-
 def launchScrapingProcess(credentials: json, mode: str) -> Dict:
     scraped_data = {}
 
@@ -46,19 +37,13 @@ def launchScrapingProcess(credentials: json, mode: str) -> Dict:
 @app.route('/', methods=['GET'])
 @app.route('/index', methods=['GET'])
 def indexPage():
-    if request.method == 'GET':
-        return {}
-
-    abort(405)
+    return {}
 
 
 @app.route('/full_scraping', methods=['POST'])
 def launchFullScraping():
     if request.method == 'POST':
-        data = request.get_data().decode('UTF-8')
-        user_creds = loadJsonDataFromRequest(data)
-
-        return launchScrapingProcess(credentials=user_creds, mode='full_scraping')
+        return launchScrapingProcess(credentials=request.get_json(), mode='full_scraping')
 
     abort(405)
 
@@ -66,10 +51,7 @@ def launchFullScraping():
 @app.route('/social_scraping', methods=['POST'])
 def launchSocialScraping():
     if request.method == 'POST':
-        data = request.get_data().decode('UTF-8')
-        user_creds = loadJsonDataFromRequest(data)
-
-        return launchScrapingProcess(credentials=user_creds, mode='social_scraping')
+        return launchScrapingProcess(credentials=request.get_json(), mode='social_scraping')
 
     abort(405)
 
@@ -77,10 +59,7 @@ def launchSocialScraping():
 @app.route('/osint_scraping', methods=['POST'])
 def launchOsintScraping():
     if request.method == 'POST':
-        data = request.get_data().decode('UTF-8')
-        user_creds = loadJsonDataFromRequest(data)
-
-        return launchScrapingProcess(credentials=user_creds, mode='osint_scraping')
+        return launchScrapingProcess(credentials=request.get_json(), mode='osint_scraping')
 
     abort(405)
 
