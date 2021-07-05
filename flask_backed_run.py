3493fa9d2505ac67910d62c348a4fd8814ba1b66
breadrock <breadrock1@gmail.com>
2021-07-05 21:01:11 +0300

    Has been added ability to launch VkScraping by vk ids list from backend

--- flask_backed_run.py
+++ flask_backed_run.py
@@ -1,6 +1,6 @@
 import json
 
-from typing import Dict
+from typing import Dict, List
 from flask import Flask, request, abort
 
 from config import HOST, PORT
@@ -10,28 +10,40 @@ from Scrapers.SraperManager import ScraperManager
 app = Flask(__name__)
 
 
-def launchScrapingProcess(credentials: json, mode: str) -> Dict:
-    scraped_data = {}
+def extractDictDataByKey(json_data: json, key: str) -> List[str]:
+    try:
+        return json_data.get(key)
+    except KeyError as e:
+        print(f'Error while parsing request json-data: {e.with_traceback()}')
+        return []
 
-    if mode == '/social_scraping':
+
+def launchScrapingProcess(credentials: json, mode: str) -> Dict:
+    if mode == 'full_scraping':
+        scraped_data = {}
+
+        scraped_data.update(
+            ScraperManager.scrapeOSINTSites(credentials=credentials)
+        )
+
         scraped_data.update(
             ScraperManager.scrapeSocialNetworks(credentials=credentials)
         )
+
+        return scraped_data
+
+    elif mode == 'vk_scraping':
+        vk_ids = extractDictDataByKey(json_data=credentials, key='ids')
+
+        return ScraperManager.scrapeVkontakte(ids=vk_ids)
 
     elif mode == 'osint_scraping':
-        scraped_data.update(
-            ScraperManager.scrapeOSINTSites(credentials=credentials)
-        )
+        return ScraperManager.scrapeOSINTSites(credentials=credentials)
 
-    else:
-        scraped_data.update(
-            ScraperManager.scrapeSocialNetworks(credentials=credentials)
-        )
-        scraped_data.update(
-            ScraperManager.scrapeOSINTSites(credentials=credentials)
-        )
+    elif mode == 'social_scraping':
+        return ScraperManager.scrapeSocialNetworks(credentials=credentials)
 
-    return scraped_data
+    return {'Error': 'Wrong request...'}
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -60,6 +72,14 @@ def launchSocialScraping():
 def launchOsintScraping():
     if request.method == 'POST':
         return launchScrapingProcess(credentials=request.get_json(), mode='osint_scraping')
+
+    abort(405)
+
+
+@app.route('/vk_scraping', methods=['POST'])
+def launchVkScraping():
+    if request.method == 'POST':
+        return launchScrapingProcess(credentials=request.get_json(), mode='vk_scraping')
 
     abort(405)
 
