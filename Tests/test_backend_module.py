9bba89c5572824b49b15e2ed940951e16aa2f375
breadrock <breadrock1@gmail.com>
2021-07-05 21:20:46 +0300

    Has been created test for vk ids

--- Tests/test_backend_module.py
+++ Tests/test_backend_module.py
@@ -2,6 +2,8 @@ import json
 
 from time import sleep
 from pathlib import Path
+from typing import Dict
+
 from requests import post
 from flask_backed_run import app
 from multiprocessing import Process
@@ -35,17 +37,16 @@ def _createProcess() -> Process:
     return Process(target=_runBackendApp, name='flask_backend_process')
 
 
-def _readUserFile() -> json:
-    file_path = Path('Users', 'yuliya_chesnokova.json').absolute()
+def _readUserFile(file_path: Path) -> json:
     user_file = open(str(file_path), 'rb')
 
     return json.load(user_file)
 
 
-def _sendRequestToScraping(mode: str) -> json:
+def _sendRequestToScraping(mode: str, json_data: Dict) -> json:
     response = post(
         url=f'http://127.0.0.1:7654/{mode}',
-        json=_readUserFile()
+        json=json_data
     )
 
     return response.json()
@@ -57,11 +58,19 @@ def test_backend_module():
 
     sleep(2)
 
+    vk_ids_file = Path('Tests', 'Users', 'vk_ids.json').absolute()
+    user_file = Path('Tests', 'Users', 'yuliya_chesnokova.json').absolute()
+
+    user_data = _readUserFile(file_path=user_file)
+    vk_ids_data = _readUserFile(file_path=vk_ids_file)
+
     try:
-        response_index = _sendRequestToScraping(mode='index')
-        response_full_scraping = _sendRequestToScraping(mode='full_scraping')
+        response_index = _sendRequestToScraping(mode='index', json_data=user_data)
+        response_vk_scraping = _sendRequestToScraping(mode='vk_scraping', json_data=vk_ids_data)
+        response_full_scraping = _sendRequestToScraping(mode='full_scraping', json_data=user_data)
 
         assert response_index == {}
+        assert response_vk_scraping == {}
         assert response_full_scraping == result_full_scraping_response
 
     except Exception as e:
